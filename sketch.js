d7ffa89acd39dab2773f64b289e5c40338c63f12
Rishith <iam.kilaru@gmail.com>
2020-10-09 15:09:36 -0400

    Added multiplayer

--- sketch.js
+++ sketch.js
@@ -1,29 +1,46 @@
-var ball;
+var ball, database, ballPos, Fosition;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database=firebase.database();
+    ballPos=database.ref("ball/position");
+    ballPos.on("value",readPos,Eerror);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if(Fosition !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-3,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(3,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-3);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+3);
+        }
+        drawSprites();
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        x: Fosition.x + x,
+        y: Fosition.y + y
+    });
+}
+
+function readPos(data){
+    Fosition = data.val();
+    ball.x = Fosition.x;
+    ball.y = Fosition.y;
+}
+
+function Eerror(){
+    console.log("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
 }
